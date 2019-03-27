ffed145cbcc52186c67cc000be60fc62a2b65cb0
masu12 <47995442+masu12@users.noreply.github.com>
2019-03-26 20:55:43 -0400

    Add Javascript
    This my first javascript code

---
+++ js/site.js
@@ -0,0 +1,17 @@
+console.log("External file");
+
+
+var city;
+var cityFrom = "";
+var cityName = "New York City";
+var x = 32;
+console.log("cityName, x:", cityName, x);
+var firstname = "Judi", lastname = "Desire";
+console.log("firstname, lastname", firstname, lastname);
+var s = "2019";
+var n = 2001;
+console.log("age:", s - n);
+console.log("age:", s + n);
+console.log("age:", s + parseInt(n) );
+var escapequotes = "This is Judi's \"city\" name";
+console.log ("escapequotes:", escapequotes);
